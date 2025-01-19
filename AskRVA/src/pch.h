679764c2bf000b04bfb321ae4d004f7e07925cac
Redbeanw44602 <redbeana44945@gmail.com>
2025-01-20 00:42:40 +0800

    chore: add pcxxheader.

---
+++ AskRVA/src/pch.h
@@ -0,0 +1,20 @@
+
+// Standard Libraries
+
+#include <exception>
+#include <functional>
+
+
+#include <fstream>
+#include <print>
+
+#include <string>
+#include <string_view>
+
+#include <unordered_map>
+#include <unordered_set>
+
+#include <type_traits>
+
+// Helper
+#include "nonstd.h"
--- AskRVA/xmake.lua
+++ AskRVA/xmake.lua
@@ -3,7 +3,7 @@ add_rules('mode.debug', 'mode.release')
 set_allowedplats('windows')
 set_allowedarchs('x64')
 
-add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
+add_repositories('liteldev-repo https://github.com/LiteLDev/xmake-repo.git')
 
 -- from xmake-repo
 add_requires('argparse      3.1')
@@ -18,6 +18,7 @@ target('askrva')
     add_includedirs('src')
     set_warnings('all')
     set_languages('c23', 'c++23')
+    set_pcxxheader('src/pch.h')
 
     add_packages(
         'argparse',
