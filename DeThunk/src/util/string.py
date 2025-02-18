17c40da90790439cb25cf468f32ca8b965710660
Redbeanw44602 <redbeana44945@gmail.com>
2025-02-18 21:36:26 +0800

    feat: enhance `find_m`, add `rfind_m`.

--- DeThunk/src/util/string.py
+++ DeThunk/src/util/string.py
@@ -2,6 +2,8 @@
 String Utiltiy
 """
 
+import re
+
 
 def startswith_m(con: str, *args) -> bool:
     for arg in args:
@@ -18,8 +20,22 @@ def endswith_m(con: str, *args) -> bool:
 
 
 def find_m(con: str, *args) -> int:
+    r_pos = -1
+    r_arg = None
     for arg in args:
         pos = con.find(arg)
-        if pos != -1:
-            return pos, arg
-    return -1, None
+        if pos != -1 and (r_pos == -1 or pos < r_pos):
+            r_pos = pos
+            r_arg = arg
+    return r_pos, r_arg
+
+
+def rfind_m(con: str, *args) -> int:
+    r_pos = -1
+    r_arg = None
+    for arg in args:
+        pos = con.rfind(arg)
+        if pos > r_pos:
+            r_pos = pos
+            r_arg = arg
+    return r_pos, r_arg
